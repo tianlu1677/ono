e63678abde77cf7ed37b4c6f4dd3826c71f10b8a
tianlu1677 <tianlu1677@gmail.com>
2018-06-20 22:35:15 +0800

    fix errors

--- src/pages/welcome.vue
+++ src/pages/welcome.vue
@@ -27,9 +27,14 @@
       <div class="share-bottom weui-panel__bd weui-flex__item">
         <div class="weui-media-box__hd bottom-container">
           <div class="weui-media-box__bd">
-            <p class="person-numebr color-pink" style="color:#ff847d; text-align: center; font-size: 14px">
-              恭喜领取成功
+            <p class="person-numebr color-pink" style="color:#ff847d; text-align: center; font-size: 20px"
+               v-if="currentAccount.status !== 'new_user' && currentAccount.status !== 'old_user'">
+              恭喜登记成功! 正在排队领取
             </p>
+            <p class="person-numebr color-pink" style="color:#ff847d; text-align: center; font-size: 20px" v-else>
+              恭喜领取成功!
+            </p>
+
             <div style="padding:0 20px">
               <x-button type="warn" @click.native="goSignIn" class="margin-top-10">
                 点此获取更多奖励
@@ -42,19 +47,25 @@
         </div>
         <div class="weui-media-box weui-media-box_appmsg share-bottom-desc">
           <div class="weui-media-box__bd">
-            <h4 style="font-size:13px;font-weight:bold;" class="weui-media-box__title color-gray">活动说明</h4>
-            <div class="font-size-16">
-              <p style="line-height: 1.5;overflow:inherit;" class="weui-media-box__desc text-left line-height-20"
-                 id="activity-desc">
-                1、本红包将于今日24点过期。（限ONO新用户领取）
-                <br>
-                2、ONO新用户（未注册用户）领取直接提取到ONO账户中，领取成功自动注册ONO账号。
-                <br>
-                3、ONO老用户（已注册用户）可通过发放区块链红包获得更多ONOT奖励。
-                <br>
-                4、本活动由ONO超级合伙人举办。
-              </p>
-            </div>
+            <a href="https://www.ono.chat" target="_blank">
+              <h4 style="font-size:15px;color:#347bfd;font-weight:bold;text-align: center"
+                  class="weui-media-box__title color-gray">
+                下载ONO应用
+              </h4>
+            </a>
+            <!--<h4 style="font-size:13px;font-weight:bold;" class="weui-media-box__title color-gray">活动说明</h4>-->
+            <!--<div class="font-size-16">-->
+            <!--<p style="line-height: 1.5;overflow:inherit;" class="weui-media-box__desc text-left line-height-20"-->
+            <!--id="activity-desc">-->
+            <!--&lt;!&ndash;1、本红包将于今日24点过期。（限ONO新用户领取）&ndash;&gt;-->
+            <!--&lt;!&ndash;<br>&ndash;&gt;-->
+            <!--&lt;!&ndash;2、ONO新用户（未注册用户）领取直接提取到ONO账户中，领取成功自动注册ONO账号。&ndash;&gt;-->
+            <!--&lt;!&ndash;<br>&ndash;&gt;-->
+            <!--&lt;!&ndash;3、ONO老用户（已注册用户）可通过发放区块链红包获得更多ONOT奖励。&ndash;&gt;-->
+            <!--&lt;!&ndash;<br>&ndash;&gt;-->
+            <!--&lt;!&ndash;4、本活动由ONO超级合伙人举办。&ndash;&gt;-->
+            <!--</p>-->
+            <!--</div>-->
           </div>
         </div>
       </div>
